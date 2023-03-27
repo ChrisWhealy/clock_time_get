d07b187d13d3079208c81090f7c772ca77c3d467
Chris Whealy <chris.whealy@red-badger.com>
2023-03-27 15:58:34 +0100

    Simplify clock id map

--- server.mjs
+++ server.mjs
@@ -2,19 +2,12 @@ import { readFileSync } from "fs"
 import { WASI } from 'wasi'
 
 const wasmFilePath = "./bin/clock_time_get.wasm"
-
-const CLOCK_ID_REALTIME = 0
-const CLOCK_ID_MONOTONIC = 1
-const CLOCK_ID_PROCESS_CPUTIME = 2
-const CLOCK_ID_THREAD_CPUTIME = 3
-
 const clockIdMap = new Map()
 
-clockIdMap.set(CLOCK_ID_REALTIME, "Realtime clock   :")
-clockIdMap.set(CLOCK_ID_MONOTONIC, "Monotonic clock  :")
-clockIdMap.set(CLOCK_ID_PROCESS_CPUTIME, "Process CPU time :")
-clockIdMap.set(CLOCK_ID_THREAD_CPUTIME, "Thread CPU time  :")
-
+clockIdMap.set(0, "Realtime clock   :")
+clockIdMap.set(1, "Monotonic clock  :")
+clockIdMap.set(2, "Process CPU time :")
+clockIdMap.set(3, "Thread CPU time  :")
 
 let wasmMem8
 
